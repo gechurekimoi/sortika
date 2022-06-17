285152e891be4f3886b6eb499493e6abf5cf212a
John Gechure Kimoi <gechurejohn@gmail.com>
2022-06-17 15:12:57 +0300

    started working on main dashboard

--- Sortika/Sortika/AssemblyInfo.cs
+++ Sortika/Sortika/AssemblyInfo.cs
@@ -1,3 +1,10 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)] 
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("Montserrat-Bold.ttf", Alias = "MontserratBold")]
+[assembly: ExportFont("Montserrat-Medium.ttf", Alias = "MontserratMedium")]
+[assembly: ExportFont("Montserrat-Regular.ttf", Alias = "MontserratRegular")]
+[assembly: ExportFont("Montserrat-SemiBold.ttf", Alias = "MontserratSemiBold")]
+[assembly: ExportFont("UIFontIcons.ttf", Alias = "UIFontIcons")] 
