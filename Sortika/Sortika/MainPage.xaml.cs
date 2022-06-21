0093c74915aab1fcb3f1ac44774b2b2d91ad5574
John Gechure Kimoi <gechurejohn@gmail.com>
2022-06-21 15:15:07 +0300

    added pie chart and some formating to the dashabord

--- Sortika/Sortika/MainPage.xaml.cs
+++ Sortika/Sortika/MainPage.xaml.cs
@@ -10,9 +10,59 @@ namespace Sortika
 {
     public partial class MainPage : ContentPage
     {
+
+
+        private List<ProjectChartData> chartData;
+        public List<ProjectChartData> ChartData
+        {
+            get { return chartData; }
+            set { chartData = value; OnPropertyChanged(); }
+        }
+
         public MainPage()
         {
             InitializeComponent();
+
+         
         }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            BindingContext = this;
+
+            PopulateChartData();
+        }
+
+
+        private void PopulateChartData()
+        {
+            try
+            {
+
+                var chartdata = new List<ProjectChartData>();
+
+                chartdata.Add(new ProjectChartData() { Description = "Fixed Income", Amount = 13 });
+                chartdata.Add(new ProjectChartData() { Description = "NSE", Amount = 15 });
+                chartdata.Add(new ProjectChartData() { Description = "Crypto-currencies", Amount = 40 });
+                chartdata.Add(new ProjectChartData() { Description = "Commodities", Amount = 22 });
+                chartdata.Add(new ProjectChartData() { Description = "High Yeild Funds", Amount = 10 });
+
+                ChartData = chartdata;
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+
+
+    public class ProjectChartData
+    {
+        public int Amount { get; set; }
+        public string Description { get; set; }
     }
 }
